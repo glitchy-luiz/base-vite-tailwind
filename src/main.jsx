8f77d773afce27a181f5100d25689c8dbd4026c9
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-09-30 22:04:23 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+function Home (){
+    return(
+        <>
+            <h1>teste</h1>
+        </>
+    )
+}
+export default Home
